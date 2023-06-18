877b9567caa2a394833a94427466fad0371e5c6a
Ram <parkeryangga@gmail.com>
2023-06-18 21:49:52 +0900

    66p ->

--- study-react/src/App.js
+++ study-react/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import React, {Fragment} from "react";
 
+// 리액트에서는 여러개의 요소를 하나의 요소로 감싸지 않으면 오류 생긴다
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const name = "할로"; //const는 ES6의 문법, 상수 선언할 때 사용
+  return(
+  // Fragment는 생략도 가능하다.
+  <>    
+  <h1>{name}  안녕?</h1>
+  <h2>잘 작동하니?</h2>
+  </>
   );
+  
 }
 
 export default App;
