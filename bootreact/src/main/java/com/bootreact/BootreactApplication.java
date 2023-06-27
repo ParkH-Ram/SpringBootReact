3dd1266abfa23efa744a2707474636146abcf3b3
ParkH-Ram <parkeryangga@gmail.com>
2023-06-27 10:34:05 +0900

    23-6-27 게시판 만들기 1. 환경설 정 및 프로젝트 설치
    스프링부트  프로젝트 설치
    리액트 프로젝트 설치

---
+++ bootreact/src/main/java/com/bootreact/BootreactApplication.java
@@ -0,0 +1,13 @@
+package com.bootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootreactApplication.class, args);
+	}
+
+}
